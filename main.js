25733e3df4c5306546d87be11038ebb9f77ba337
Yash Maheshwari <yash.maheshwari1@ibm.com>
2026-01-29 12:14:20 +0530

    feat(tutorial): complete step 1

--- main.js
+++ main.js
@@ -1,24 +1,66 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import './style.scss';
+import '@carbon/web-components/es/components/button/button.js';
+import '@carbon/web-components/es/components/ui-shell/index';
+import '@carbon/web-components/es/components/checkbox/index';
+import '@carbon/web-components/es/components/content-switcher/index';
+import '@carbon/web-components/es/components/skip-to-content/index.js';
+const bodyEl = document.querySelector('body');
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+// button click handler
+// const handleClick = () => {
+//   bodyEl.classList.toggle('g10');
+//   bodyEl.classList.toggle('g100');
+// };
+// document.querySelector('.button').addEventListener('click', handleClick);
 
-setupCounter(document.querySelector('#counter'))
+// set initial theme based on preferences
+// if (matchMedia('prefers-color-scheme').matches) {
+//   bodyEl.classList.add('g100');
+// } else {
+//   bodyEl.classList.add('g10');
+// }
+
+const handleSwitch = (ev) => {
+  // Applies new theme or defers to system preferences by removing theme
+  switch (ev.detail.item.value) {
+    case 'light':
+      bodyEl.classList.remove('g100');
+      bodyEl.classList.add('g10');
+      break;
+    case 'dark':
+      bodyEl.classList.remove('g10');
+      bodyEl.classList.add('g100');
+      break;
+    default:
+      bodyEl.classList.remove('g10');
+      bodyEl.classList.remove('g100');
+  }
+};
+document
+  .querySelector('.theme-selector')
+  .addEventListener('cds-content-switcher-selected', handleSwitch);
+
+const handleHeaderCompliment = (ev) => {
+  document
+    .querySelector('cds-header')
+    .classList.toggle('compliment', ev.target.checked);
+};
+document
+  .querySelector('.theme-header__compliment')
+  .addEventListener('cds-checkbox-changed', handleHeaderCompliment);
+
+// this is done to handel that only one panel is open at time 
+const handleGlobalActionClick = (ev) => {
+  const targetPanelId = ev.currentTarget.getAttribute('panel-id');
+  const panels = document.querySelectorAll('cds-header-panel');
+  // check to see if other panels are open and close them
+  panels.forEach((panel) => {
+    if (panel.id !== targetPanelId) {
+      panel.expanded = false;
+    }
+  });
+};
+const globalActions = document.querySelectorAll('cds-header-global-action');
+[...globalActions].forEach((action) =>
+  action.addEventListener('click', handleGlobalActionClick)
+);
